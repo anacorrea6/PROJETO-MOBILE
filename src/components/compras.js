7dd0e0571d3c3dade0505e573ecbe7c8840130b8
Ana  Corrêa <ana.j.ferraz6@aluno.senai.br>
2026-08-26 15:21:43 -0300

    atualização

---
+++ src/components/compras.js
@@ -0,0 +1,12 @@
+import { useState } from "react" ;
+import {
+   StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+} from "react-native";
+
+export default function ComprasItem({
+    
+})
